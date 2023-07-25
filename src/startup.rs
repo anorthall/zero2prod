bfea814599173083e5df1609abba3ee0890ffec6
Andrew Northall <andrew@northall.me.uk>
2023-07-25 17:59:04 +0100

    Add telemetry functionality

--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,20 @@
-use std::net::TcpListener;
-
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpServer};
-use sqlx::PgPool;
-
 use crate::routes::{health_check, subscribe};
 
+use actix_web::dev::Server;
+use actix_web::web::Data;
+use actix_web::{web, App, HttpServer};
+use tracing_actix_web::TracingLogger;
+
+use sqlx::PgPool;
+
+use std::net::TcpListener;
+
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    let connection = web::Data::new(db_pool);
+    let connection = Data::new(db_pool);
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection.clone())
